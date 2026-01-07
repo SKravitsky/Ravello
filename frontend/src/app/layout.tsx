0731d3d4b5fb9a9011fe96f90b9334f90382be9c
Steven Kravitsky <steven.kravitsky@gmail.com>
2026-01-06 23:33:19 -0500

    Fix forms in two different ways

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -31,6 +31,23 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className={`${fontPrimary.variable} ${fontSecondary.variable}`}>
+                {/* Hidden form for Netlify to detect at build time */}
+                <form
+                    name="rsvp"
+                    data-netlify="true"
+                    netlify-honeypot="bot-field"
+                    hidden
+                >
+                    <input type="hidden" name="form-name" value="rsvp" />
+                    <input name="bot-field" />
+                    <input name="name" />
+                    <input name="email" />
+                    <input name="attendance" />
+                    <input name="guestCount" />
+                    <input name="dietaryRestrictions" />
+                    <textarea name="message" />
+                </form>
+
                 <PasswordProtection>
                     <header className={styles.header}>
                         <Navbar />
